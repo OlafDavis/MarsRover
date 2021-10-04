a8c7b4c2ec178bfe3f56f23b33383c31e8f64262
Olaf Davis <olaf.davis@softwire.com>
2021-10-04 08:54:35 +0100

    Added a template app

---
+++ MarsDemo/MarsRoverDemo/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MarsRoverDemo
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
